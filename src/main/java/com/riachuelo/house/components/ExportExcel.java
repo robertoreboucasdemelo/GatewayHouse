2e20ea764cf73bdf0f101ad42ffe56a864f0c9c0
Roberto Melo <robertoreboucasdemelo@gmail.com>
2022-01-06 22:04:26 -0300

    Refactory Excel File Class

--- src/main/java/com/riachuelo/house/components/ExportExcel.java
+++ src/main/java/com/riachuelo/house/components/ExportExcel.java
@@ -15,88 +15,119 @@ import org.apache.poi.ss.usermodel.HorizontalAlignment;
 import org.apache.poi.ss.usermodel.IndexedColors;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.VerticalAlignment;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import com.riachuelo.house.constants.Constants;
+import com.riachuelo.house.exceptions.ResourceNotFoundException;
 import com.riachuelo.house.models.ItemError;
 
 @Component
 public class ExportExcel {
 	
-	public void generate(Inconsistency inconsistency) {
+	private static final Logger LOGGER = LoggerFactory.getLogger(ExportExcel.class);
+	
+	@Value("${app.house.file.output.path}")
+    private String path;
+	HSSFWorkbook workbook = new HSSFWorkbook();
+	HSSFSheet sheet = workbook.createSheet(Constants.SHEET);
+
+	Cell cell;
+	Row  row;
+	
+	int rownum  = 0;
+	int cellnum = 0;
+	
+	public void generate(Inconsistency inconsistency) {	
 		
-		int rownum = 0;
-		int cellnum = 0;
-		Cell cell;
-		Row row;
+		LOGGER.info(Constants.STEP_CREATE_FILE);
 		
-		HSSFWorkbook workbook = new HSSFWorkbook();
-		HSSFSheet sheet = workbook.createSheet(Constants.SHEET);
-		
+		this.config();
+		this.loadHeader();
+		this.loadItens(inconsistency);
+		this.writeFile();
+	}
+	
+	public void config() {
 		sheet.setDefaultColumnWidth(15);
 		sheet.setDefaultRowHeight((short)400);
-
+	}
+	
+	public CellStyle generateHeaderStyle() {
 		CellStyle headerStyle = workbook.createCellStyle();
 		headerStyle.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
 		headerStyle.setFillPattern(FillPatternType.SOLID_FOREGROUND);
 		headerStyle.setAlignment(HorizontalAlignment.CENTER);
 		headerStyle.setVerticalAlignment(VerticalAlignment.CENTER);
 
+		return headerStyle;
+	}
+	
+	public CellStyle generateTextStyle() {
 		CellStyle textStyle = workbook.createCellStyle();
 		textStyle.setAlignment(HorizontalAlignment.CENTER);
 		textStyle.setVerticalAlignment(VerticalAlignment.CENTER);
 		
+		return textStyle;
+	}
+	
+	public void loadHeader() {	
 		row = sheet.createRow(rownum++);
 		
 		cell = row.createCell(cellnum++);
-		cell.setCellStyle(headerStyle);
-		cell.setCellValue("Arquivo");
+		cell.setCellStyle(generateHeaderStyle());
+		cell.setCellValue(Constants.LABEL_FILE);
 		
 		cell = row.createCell(cellnum++);
-		cell.setCellStyle(headerStyle);
-		cell.setCellValue("Matricula");
+		cell.setCellStyle(generateHeaderStyle());
+		cell.setCellValue(Constants.LABEL_REGISTRATION);
 		
 		cell = row.createCell(cellnum++);
-		cell.setCellStyle(headerStyle);
-		cell.setCellValue("Vendedor");
+		cell.setCellStyle(generateHeaderStyle());
+		cell.setCellValue(Constants.LABEL_SALESMAN);
 		
 		cell = row.createCell(cellnum++);
-		cell.setCellStyle(headerStyle);
-		cell.setCellValue("Inconsistencia");
-		
+		cell.setCellStyle(generateHeaderStyle());
+		cell.setCellValue(Constants.LABEL_ERROR);		
+	}
+	
+	public void loadItens(Inconsistency inconsistency) {
 		for (ItemError error : inconsistency.getInconsistencies()) {
 			row = sheet.createRow(rownum++);
 			cellnum = 0;
 
 			cell = row.createCell(cellnum++);
-			cell.setCellStyle(textStyle);
+			cell.setCellStyle(generateTextStyle());
 			cell.setCellValue(error.getFile());
 
 			cell = row.createCell(cellnum++);
-			cell.setCellStyle(textStyle);
+			cell.setCellStyle(generateTextStyle());
 			cell.setCellValue(error.getRegistration());
 			
 			cell = row.createCell(cellnum++);
-			cell.setCellStyle(textStyle);
+			cell.setCellStyle(generateTextStyle());
 			cell.setCellValue(error.getSalesman());
 			
 			cell = row.createCell(cellnum++);
-			cell.setCellStyle(textStyle);
+			cell.setCellStyle(generateTextStyle());
 			cell.setCellValue(error.getDescription());
 		
 		}
-		
+	}
+	
+	public void writeFile() {
 		try {
-			// Escrevendo o arquivo em disco
-			FileOutputStream out = new FileOutputStream(new File("C:\\Users\\user\\Documents\\Riachuelo\\files\\products.xls"));
+			FileOutputStream out = new FileOutputStream(new File(path));
 			workbook.write(out);
 			out.close();
 			workbook.close();
-			System.out.println("Success!!");
+			LOGGER.info(Constants.STEP_SUCCESS_FILE);
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			throw new ResourceNotFoundException(Constants.FILE_NOT_FOUND);
 		} catch (IOException e) {
-			e.printStackTrace();
+			throw new ResourceNotFoundException(Constants.IO_ERROR);
 		}
 	}
 
--- src/main/java/com/riachuelo/house/constants/Constants.java
+++ src/main/java/com/riachuelo/house/constants/Constants.java
@@ -3,16 +3,22 @@ package com.riachuelo.house.constants;
 public class Constants {
 
 	public static final String  RESOURCE_NOT_FOUND = "Resource Not Found";
-	public static final String  FILE_NOT_FOUND = "File Not Found";
-	public static final String  IO_ERROR = "IO Error";
-	public static final String  MONETARY_SYMBOL = "R$";
-	public static final String  PATH = "";
-	public static final String  STEP_READER_SALES = "Step 01 - Reader Commission Sales File";
-	public static final String  STEP_READER_GOALS = "Step 02 - Reader Commission Goals File";
-	public static final String  FILE_SALES = "Vendas Comissionadas";
-	public static final String  FILE_GOALS = "Metas Comissionadas";
-	public static final String  ERROR_GOALS = "Dados das Metas Faltando ou Fora do Padrão";
-	public static final String  ERROR_SALES = "Dados das Vendas Faltando ou Fora do Padrão";
-	public static final String  SHEET       = "Inconsistencias";
+	public static final String  FILE_NOT_FOUND     = "File Not Found";
+	public static final String  IO_ERROR           = "IO Error";
+	public static final String  MONETARY_SYMBOL    = "R$";
+	public static final String  PATH               = "";
+	public static final String  STEP_READER_SALES  = "Step 01 - Reader Commission Sales File";
+	public static final String  STEP_READER_GOALS  = "Step 02 - Reader Commission Goals File";
+	public static final String  STEP_CREATE_FILE   = "Step 04 - Creating Inconsistency Output File";
+	public static final String  STEP_SUCCESS_FILE  = "Step 05 - File Created Successfully";
+	public static final String  FILE_SALES         = "Vendas Comissionadas";
+	public static final String  FILE_GOALS         = "Metas Comissionadas";
+	public static final String  ERROR_GOALS        = "Dados das Metas Faltando ou Fora do Padrão";
+	public static final String  ERROR_SALES        = "Dados das Vendas Faltando ou Fora do Padrão";
+	public static final String  SHEET              = "Inconsistencias";
+	public static final String  LABEL_FILE         = "Arquivo";
+	public static final String  LABEL_REGISTRATION = "Matrícula";
+	public static final String  LABEL_SALESMAN     = "Vendedor";
+	public static final String  LABEL_ERROR        = "Inconsistência";
 	
 }
